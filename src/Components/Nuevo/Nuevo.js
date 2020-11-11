5c35c33a3bf5fd206c117483e5a12653b4fb754a
majogw <najogc@gmail.com>
2020-11-11 09:54:35 +0100

    debug

--- src/Components/Nuevo/Nuevo.js
+++ src/Components/Nuevo/Nuevo.js
@@ -4,10 +4,10 @@ import {BigComponent} from './BigComponent';
 export const Nuevo = () => {
   return (
     <div>
-      <BigComponent color={'#4ac5ff'} arrow />
-      <BigComponent color={'white'} activeCircle={1} />
-      <BigComponent color={'#4ac5ff'} activeCircle={2} />
-      <BigComponent color={'white'} activeCircle={3} />
+      <BigComponent component={1} color={'#4ac5ff'} arrow />
+      <BigComponent component={2} color={'white'} activeCircle={1} />
+      <BigComponent component={3} color={'#4ac5ff'} activeCircle={2} />
+      <BigComponent component={4} color={'white'} activeCircle={3} />
     </div>
   );
 };
